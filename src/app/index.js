cc3d3c8f5925a4243026c5d5a396f2e3dc8580fe
Karel van Doesburg <karelvandoesburg@gmail.com>
2017-12-17 22:13:31 +0100

    logo wordt er door index ingeladen

--- src/app/index.js
+++ src/app/index.js
@@ -4,4 +4,4 @@ import ScreenStart from './ScreenStart';
 import Logo from './Logo';
 
 ReactDOM.render(<Logo company="ergatis"/>,document.getElementById('logo-container'));
-ReactDOM.render(<ScreenStart company="ergatis"/>,document.getElementById('wrapper'));
+ReactDOM.render(<ScreenStart company="ergatis"/>,document.getElementById('context-container'));
